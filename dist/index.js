80fbde5b7767e349eeedaf96632e759d051ce9b6
Márton Visnovitz <vimtaai@inf.elte.hu>
2016-06-21 11:34:33 +0000

    Rebuild sources

--- dist/index.js
+++ dist/index.js
@@ -22,13 +22,18 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-/** Class representing the dispatcher in the Flux architecture */
+/** 
+ * Class representing the dispatcher in the Flux architecture
+ * @extends {external:EventEmitter}
+ */
 
 var Fluxbase = function (_EventEmitter) {
     _inherits(Fluxbase, _EventEmitter);
 
     /**
-     * Create a Fluxbase object
+     * Create a Fluxbase object. (Should not be called. The module exports an instance.)
+     * 
+     * @constructor
      */
 
     function Fluxbase() {
@@ -41,10 +46,18 @@ var Fluxbase = function (_EventEmitter) {
     } // END constructor
 
     /**
-     * Broadcast an action to all Stores
+     * Broadcast an action to all Stores.
+     * 
      * @param {Object} action      - The action object
      * @param {string} action.type - The type of the action
      * @param {*=}     action.data - Additional data for the action
+     * @exception {Error} Will throw an error if the action parameter has no `type` property
+     * @example
+     * ```js
+     * Fluxbase.dispatch({
+     *   type: 'next'
+     * });
+     * ```
      */
 
 
@@ -61,8 +74,29 @@ var Fluxbase = function (_EventEmitter) {
         } // END dispatch
 
         /**
-         * Create a new Store and register it for broadcasts
-         * @param {(Object|Firebase.app.App)} obj - The initializer of the Store
+         * Create a new Store and register it for broadcasts.
+         * 
+         * @access public
+         * @param {(Object|Firebase.app)=} obj - The initializer of the Store
+         * @return {Store} A new Store object
+         * @example
+         * With an object literal:
+         * ```js
+         * var store = Fluxbase.createStore({
+         *   apiKey: "<your-api-key>",
+         *   databaseURL: "<your-database-url>"
+         * });
+         * ```
+         * 
+         * With a Firebase.app object:
+         * ```js
+         * var app = Firebase.initializeApp({
+         *   apiKey: "<your-api-key>",
+         *   databaseURL: "<your-database-url>",
+         *}, 'appname');
+         *
+         * var store = Fluxbase.createStore(app);
+         * ```
          */
 
     }, {
@@ -78,14 +112,22 @@ var Fluxbase = function (_EventEmitter) {
     return Fluxbase;
 }(_events.EventEmitter);
 
-/**  Class representing the store in the Flux architecture */
+/** 
+ * Class representing the store in the Flux architecture 
+ * @extends {external:EventEmitter}
+ * @fires Store#connect
+ * @fires Store#disconnect
+ * @fires Store#reconnect
+ */
 
 
 var Store = function (_EventEmitter2) {
     _inherits(Store, _EventEmitter2);
 
     /**
-     * Create a Store object
+     * Create a Store object. (Should not be used. Use {@link Fluxbase#createStore} instead.)
+     * 
+     * @constructor
      * @param {(Object|Firebase.app.App)} obj - The initializer of the Store
      */
 
@@ -99,11 +141,18 @@ var Store = function (_EventEmitter2) {
         return _this2;
     } // END constructor
 
-    // init :: object? -> void
     /**
-     * Initialize the Store
-     * Should be called only on an uninitialized Store
+     * Initialize an uninitialized Store.
+     * Should be called only on an uninitialized Store.
      * @param {(Object|Firebase.app.App)=} obj - The initializer of the Store
+     * @example
+     * ```js
+     * var store = Fluxbase.createStore(); // Uninitialized Store
+     * store.init({
+     *   apiKey: "<your-api-key>",
+     *   databaseURL: "<your-database-url>",   
+     * });
+     * ```
      */
 
 
@@ -116,7 +165,7 @@ var Store = function (_EventEmitter2) {
 
             if (obj !== undefined) {
                 // Setting up Firebase app
-                if (obj instanceof _firebase2.default.app) {
+                if (obj instanceof _firebase2.default.app.App) {
                     this._app = obj;
                 } else if ((typeof obj === 'undefined' ? 'undefined' : _typeof(obj)) === 'object') {
                     this._app = _firebase2.default.initializeApp(obj, 'store' + new Date().getTime());
@@ -147,7 +196,9 @@ var Store = function (_EventEmitter2) {
     return Store;
 }(_events.EventEmitter);
 
-// Module export: the Flux dispatcher
-
-
 exports.default = new Fluxbase();
+
+/**
+ * @external EventEmitter
+ * @see https://nodejs.org/api/events.html#events_class_eventemitter
+ */
